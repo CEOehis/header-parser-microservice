e0825121c265e829ff931036383b7b54998812a9
ceoehis <ceo.ehis@outlook.com>
2018-05-06 19:40:02 +0100

    add homepage and corresponding routes

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ var app = express();
 
 var port = process.env.PORT || 3000;
 
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'pug');
+
+app.get('/', function(req, res) {
+  var projectUrl = req.protocol + '://' + req.get('host');
+  res.render('index', { title: 'Header Parser Microservice', projectUrl });
+});
+
 app.get('/api/whoami', function(req, res) {
   var userDetails = {
     ipaddress: req.ip || req.connection.remoteAddress || req.socket.remoteAddress,
@@ -10,7 +18,12 @@ app.get('/api/whoami', function(req, res) {
     software: req.headers['user-agent'],
   }
   res.send(userDetails);
-})
+});
+
+// redirect other routes to index page for usage instructions
+app.get('*', function(req, res) {
+  res.redirect('/');
+});
 
 app.listen(port, function(err) {
   if(err) throw err;
