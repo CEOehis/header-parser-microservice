24f1aa966098162b2fcb5941ffb4579b2e7626b7
ceoehis <ceo.ehis@outlook.com>
2018-05-07 04:08:07 +0100

    use header('x-forwarded-for') to get a proxied ip

--- server.js
+++ server.js
@@ -6,14 +6,14 @@ var port = process.env.PORT || 3000;
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'pug');
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   var projectUrl = req.protocol + '://' + req.get('host');
   res.render('index', { title: 'Header Parser Microservice', projectUrl });
 });
 
-app.get('/api/whoami', function(req, res) {
+app.get('/api/whoami', function (req, res) {
   var userDetails = {
-    ipaddress: req.ip || req.connection.remoteAddress || req.socket.remoteAddress,
+    ipaddress: req.header('x-forwarded-for') || req.connection.remoteAddress || req.socket.remoteAddress,
     language: req.headers['accept-language'].slice(0, req.headers['accept-language'].indexOf(',')),
     software: req.headers['user-agent'],
   }
@@ -21,11 +21,11 @@ app.get('/api/whoami', function(req, res) {
 });
 
 // redirect other routes to index page for usage instructions
-app.get('*', function(req, res) {
+app.get('*', function (req, res) {
   res.redirect('/');
 });
 
-app.listen(port, function(err) {
-  if(err) throw err;
+app.listen(port, function (err) {
+  if (err) throw err;
   console.log('Server started. Listening on port: ', port);
 })
